7a28be1eb332de403a7fd034ae890e0307595ad0
Erick <hearteric57@gmail.com>
2023-10-29 10:13:02 +0300

    feat: login

--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:writopia_frontend/constants/constants.dart';
 import 'package:writopia_frontend/models/user.dart';
 import 'package:http/http.dart';
@@ -21,6 +22,32 @@ Future<User?> signUpUser(User u) async {
       json.decode(response.body)["message"],
     );
   } catch (e) {
+    showErrorMessage(
+      "Exception",
+      e.toString(),
+    );
+  }
+  return null;
+}
+
+Future<User?> loginUser(User u) async {
+  try {
+    var response = await post(
+      Uri.parse("$urlPrefix/users/auth"),
+      headers: defaultHeader,
+      body: json.encode(u.toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      return User.fromJson(json.decode(response.body));
+    }
+
+    showErrorMessage(
+      "Verification Error",
+      json.decode(response.body)["message"],
+    );
+  } catch (e) {
+    debugPrint(e.toString());
     showErrorMessage(
       "Exception",
       e.toString(),
